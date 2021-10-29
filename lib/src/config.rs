e7619de739c266e71f386102334b09d0b7970acb
Kirill Kirillov <kirusfg@gmail.com>
2021-10-29 19:04:02 +0500

    Makes the app store the config and the database in the OS-specific default config directory

--- lib/src/config.rs
+++ lib/src/config.rs
@@ -1,23 +1,109 @@
-use std::path::PathBuf;
+use std::{
+    fs::{create_dir_all, File},
+    io::Write,
+    path::{Path, PathBuf},
+};
 
-use config::{Config, ConfigError, Environment, File};
+use config::{Config as AppConfig, ConfigError, Environment, File as ConfigFile};
 use directories::ProjectDirs;
+use serde::{Deserialize, Serialize};
+use toml::to_vec;
 
-/// Returns a `Config` from the OS's default config directory
-///
-/// # Errors
-///
-/// Returns `ConfigError` if either reading the config file or
-/// the environment has failed
-pub fn get_config() -> Result<Config, ConfigError> {
-    let config_path: PathBuf = ProjectDirs::from("", "", "bookshelf")
-        .unwrap()
-        .config_dir()
-        .join("config.toml");
+#[derive(Deserialize, Serialize)]
+pub struct Database {
+    pub path: String,
+}
 
-    let mut config: Config = Config::default();
-    config.merge(File::from(config_path))?;
-    config.merge(Environment::with_prefix("BOOKSHELF"))?;
+impl Default for Database {
+    fn default() -> Self {
+        let path: String = ProjectDirs::from("", "", "bookshelf")
+            .unwrap()
+            .config_dir()
+            .join("db")
+            .as_os_str()
+            .to_str()
+            .unwrap()
+            .to_string();
 
-    Ok(config)
+        Database { path }
+    }
+}
+
+#[derive(Default, Deserialize, Serialize)]
+pub struct Config {
+    pub db: Database,
+}
+
+impl Config {
+    /// Returns a `Config` with settings from config.toml located in the
+    /// OS's default config directory merged with the environment variables.
+    /// If it is not created yet, initializes the config with default values.
+    ///
+    /// # Errors
+    ///
+    /// Returns `ConfigError` if either reading the config file or
+    /// the environment has failed
+    pub fn get_or_default() -> Result<Self, ConfigError> {
+        match Self::exists() {
+            true => Self::get(),
+            false => Self::init(),
+        }
+    }
+
+    /// Returns a `Config` with settings from config.toml located in the
+    /// OS's default config directory merged with the environment variables
+    ///
+    /// # Errors
+    ///
+    /// Returns `ConfigError` if either reading the config file or
+    /// the environment has failed
+    fn get() -> Result<Self, ConfigError> {
+        let config_path: PathBuf = ProjectDirs::from("", "", "bookshelf")
+            .unwrap()
+            .config_dir()
+            .join("config.toml");
+
+        let mut app_config: AppConfig = AppConfig::default();
+        app_config.merge(ConfigFile::from(config_path)).unwrap();
+        app_config
+            .merge(Environment::with_prefix("BOOKSHELF"))
+            .unwrap();
+
+        Ok(app_config.try_into().unwrap())
+    }
+
+    /// Writes a default `Config` into config.toml located at the
+    /// OS's default config directory, and returns it.
+    ///
+    /// # Panics
+    ///
+    /// Panics if there is some IO error
+    fn init() -> Result<Self, ConfigError> {
+        // Creating a config folder "bookshelf" if it doesn't exist yet
+        let dirs: ProjectDirs = ProjectDirs::from("", "", "bookshelf").unwrap();
+        let config_folder: &Path = dirs.config_dir();
+        create_dir_all(config_folder).unwrap();
+
+        // Creating a Config with default values
+        let config: Config = Config::default();
+
+        // Creating a config.toml file with default settings if it doesn't exist yet
+        let config_file_path: PathBuf = config_folder.join("config.toml");
+        let mut config_file: File = File::create(config_file_path).unwrap();
+
+        // Writing the default Config to config.toml
+        let config_toml = to_vec(&config).unwrap();
+        config_file.write_all(&config_toml).unwrap();
+
+        Ok(config)
+    }
+
+    /// Returns whether the config folder and config.toml exist
+    fn exists() -> bool {
+        ProjectDirs::from("", "", "bookshelf")
+            .unwrap()
+            .config_dir()
+            .join("config.toml")
+            .exists()
+    }
 }
--- lib/src/shelf.rs
+++ lib/src/shelf.rs
@@ -1,16 +1,13 @@
-use crate::config::get_config;
+use crate::config::Config;
 use crate::entry::Entry;
 
-use config::Config;
-
 use bincode::{deserialize, serialize};
-use directories::ProjectDirs;
 use serde::{Deserialize, Serialize};
 use std::{
     collections::HashSet,
     fs::File,
     io::{Error, Read, Write},
-    path::{Path, PathBuf},
+    path::PathBuf,
 };
 
 #[derive(Default, Deserialize, Serialize)]
@@ -27,36 +24,39 @@ impl Shelf {
         self.entries.remove(&entry);
     }
 
+    pub fn init() -> Result<(), Error> {
+        let config: Config = Config::get_or_default().unwrap();
+
+        let db_file: PathBuf = PathBuf::from(config.db.path);
+
+        let shelf: Shelf = Shelf::default();
+        let binary_data: Vec<u8> = serialize(&shelf).unwrap();
+
+        let mut db_file = File::create(db_file).unwrap();
+        db_file.write_all(&binary_data)
+    }
+
     pub fn write_to_file(&self) -> Result<(), Error> {
-        let config: Config = get_config().unwrap();
-        let dirs = ProjectDirs::from("", "", "bookshelf").unwrap();
-        let config_path: &Path = dirs.config_dir();
+        let config: Config = Config::get_or_default().unwrap();
 
-        let file: String = match config.get_str("db") {
-            Ok(f) => f,
-            Err(_) => String::from("db"),
-        };
+        let db_file: PathBuf = PathBuf::from(config.db.path);
 
-        let db_file: PathBuf = config_path.join(file);
-
-        let binary_data: Vec<u8> = serialize(&self.entries).unwrap();
+        let binary_data: Vec<u8> = serialize(&self).unwrap();
 
         let mut db_file = File::create(db_file).unwrap();
         db_file.write_all(&binary_data)
     }
 
     pub fn read_from_file() -> Result<Shelf, Error> {
-        let config: Config = get_config().unwrap();
-        let dirs = ProjectDirs::from("", "", "bookshelf").unwrap();
-        let config_path: &Path = dirs.config_dir();
+        let config: Config = Config::get_or_default().unwrap();
 
-        let file: String = match config.get_str("db") {
-            Ok(f) => f,
-            Err(_) => String::from("db"),
-        };
+        let db_file: PathBuf = PathBuf::from(config.db.path);
 
-        let db_file: PathBuf = config_path.join(file);
+        if !db_file.exists() {
+            Self::init()?;
+        }
 
+        println!("{}", db_file.to_str().unwrap());
         let mut binary_data: Vec<u8> = Vec::new();
 
         let mut db_file = File::open(db_file).unwrap();
