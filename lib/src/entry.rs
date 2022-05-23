d01d76b2b3f6be2395180b26753e6025798d5cb7
Kirill Kirillov <kirusfg@gmail.com>
2022-05-23 11:33:32 +0500

    Add an Entry builder

--- lib/src/entry.rs
+++ lib/src/entry.rs
@@ -7,10 +7,8 @@ use std::{hash::Hash, path::PathBuf};
 /// a book, an article, or a png file of a poster/infographic. This
 /// path must be unique to be stored on a shelf - no duplicates
 /// are allowed.
-#[derive(Clone, Deserialize, Eq, Serialize)]
+#[derive(Clone, Default, Deserialize, Eq, Serialize)]
 pub struct Entry {
-    /// Entry id
-    pub id: u64,
     /// Path to the entry (can be a link), must be unique
     pub path: PathBuf,
     /// Path to an optional bibliography entry in BibTeX format
@@ -40,6 +38,41 @@ impl PartialEq for Entry {
     }
 }
 
+impl Entry {
+    /// Creates a new [`Entry`] form a path
+    pub fn new(path: PathBuf) -> Self {
+        if !path.exists() {
+            panic!("The file {:?} does not exist!", path);
+        }
+
+        Entry {
+            path,
+            ..Default::default()
+        }
+    }
+
+    /// Links a BibTeX bibliography to the entry
+    ///
+    /// # Panics
+    ///
+    /// This function panics if the path to a BibTeX file provided is invalid
+    /// (either the file does not exists, or it has a wrong extension)
+    pub fn with_bib(mut self, bib_path: PathBuf) -> Self {
+        if !bib_path.exists() {
+            panic!("The file {:?} does not exist!", bib_path);
+        }
+
+        let ext = bib_path.extension();
+        if ext.is_none() || ext.unwrap().to_str().unwrap() != "bib" {
+            panic!("The file {:?} is not a BibTeX file!", bib_path);
+        }
+
+        self.bib_path = Some(bib_path);
+
+        self
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use crate::entry::*;
@@ -52,14 +85,12 @@ mod tests {
     #[test]
     fn entries_with_same_paths() {
         let entry1 = Entry {
-            id: 1,
             path: PathBuf::from("./harry_potter.pdf"),
             bib_path: None,
             tags: Some(vec![String::from("fantasy"), String::from("favorite")]),
         };
 
         let entry2 = Entry {
-            id: 2,
             path: PathBuf::from("./not_harry_potter.pdf"),
             bib_path: None,
             tags: Some(vec![
